2666d12602795ece6c6b83c9ec4e568a19a55420
Operator <glebgizko@gmail.com>
2025-10-04 14:05:27 +0300

    04.10.2025: Представление функций компонентов: GetDataNullptrEngine, CreateObjectNullptrEngine, MemoryManagementNullptrEngine

--- app/src/main/cpp/Component/StartupNullptrEngineSource.cpp
+++ app/src/main/cpp/Component/StartupNullptrEngineSource.cpp
@@ -6,6 +6,44 @@ StartupNullptrEngine::StartupNullptrEngine(ANativeWindow* Window)
     this->StartupNullptrEnginePhysicalDevice();
     this->StartupNullptrEngineSurface(Window);
     this->StartupNullptrEngineDevice();
+
+    //Привер выделения памяти для устройства
+    VkDeviceMemory Memory = {};
+    MemoryManagement.SetMemoryDevice(Device, 1024*1024, 0, Memory);
+
+    //Пример создания CommandBuffer - командного буфера. И подачи команд из командного буфера на выполение
+    VkBuffer Buffer = {};
+    VkCommandPool Pool = {};
+    VkCommandBuffer CommandBuffer = {};
+    VkQueue Queue = {};
+    NewObject.CreateBuffer(Device, Buffer, 1024, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE); //Создание буфера
+
+    //Если нужно привязать буфер к выделеной памяти, то:
+    VkMemoryRequirements BufferMemoryRequirements = {};
+    MemoryManagement.GetBufferMemoryRequirements(Device, Buffer, BufferMemoryRequirements); //Получений требований по памяти для буфера
+    MemoryManagement.BindBufferMemory(Device, Memory, Buffer, BufferMemoryRequirements.size); //Привязка буфера к выделеной памяти
+
+    NewObject.CreateCommandPool(Device, GraphicsQueueFamily, VK_COMMAND_POOL_CREATE_TRANSIENT_BIT, Pool); //Создание командного пула
+    NewObject.AllocationCommandBuffer(Device, CommandBuffer, Pool, VK_COMMAND_BUFFER_LEVEL_PRIMARY, 1); //Выделение командного буфера
+    //Запись команды в командный буфер
+    NewObject.WriteCommandBuffer(CommandBuffer, VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT, Buffer, Buffer, 1, 0, 0, 1024);
+    Data.GetQueue(Device, Queue, GraphicsQueueFamily, 1); //Получение потока
+    //Подача команды на выполнение
+    Data.CommandSubmit(CommandBuffer, Queue, VK_NULL_HANDLE, 0, 0, 0, 1, 1, 0, 0);
+
+    //Пример создания Image - изображения
+    VkImage Image = {};
+    Data.GetFormat(PhysicalDevice, Surface, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR, VK_FORMAT_B8G8R8_SRGB); //Получение формата
+    //Создание изображения
+    NewObject.CreateImage(Device, Image, VK_IMAGE_TYPE_2D, Data.Formats.format, VkExtent3D{.width=512,.height=512,.depth=1}, 0, 0, VK_SAMPLE_COUNT_1_BIT,
+                          VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, VK_SHARING_MODE_EXCLUSIVE);
+
+    //Пример создание ImageView
+    VkImageView ImageView = {};
+    //Создание ImageView
+    NewObject.CreateImageView(Device, ImageView, Image, VK_IMAGE_VIEW_TYPE_2D, Data.Formats.format,
+                              VkComponentMapping{.r=VK_COMPONENT_SWIZZLE_IDENTITY,.g=VK_COMPONENT_SWIZZLE_IDENTITY,.b=VK_COMPONENT_SWIZZLE_IDENTITY,.a=VK_COMPONENT_SWIZZLE_IDENTITY},
+                              VkImageSubresourceRange{.aspectMask=VK_IMAGE_ASPECT_COLOR_BIT,.baseMipLevel=0,.levelCount=0,.baseArrayLayer=0,.layerCount=0});
 }
 
 void StartupNullptrEngine::StartupNullptrEngineInstance()
